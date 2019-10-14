c49b8d9917ba65869a6572130a0c8935d566b0b2
tk6996 <watcharinkanha@gmail.com>
2019-10-14 21:57:31 +0700

    commit

--- Matlab/mySetIdentity.m
+++ Matlab/mySetIdentity.m
@@ -1,11 +1,4 @@
 function [ X_out ] = mySetIdentity( X )
-    if size(X,1) == size(X,2)
-        X_out = mySetZero(X);
-        for index = 1:length(X)
-            X_out(index,index) = 1;
-        end
-    else
-        X_out = 'FAILED';
-    end
+    X_out = eye(size(X));
 end
 
