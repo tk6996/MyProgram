bf27f01fef46acec5d198c8c0afe554f35403883
tk6996 <watcharinkanha@gmail.com>
2019-10-16 22:42:31 +0700

    commit

---
+++ Java/BinarySearchTree.java
@@ -0,0 +1,264 @@
+import java.util.Comparator;
+
+public class BinarySearchTree<T> {
+    protected class TreeNode {
+        private T data;
+        private TreeNode left;
+        private TreeNode right;
+
+        public TreeNode(T data, TreeNode left, TreeNode right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        public TreeNode(T data) {
+            this(data, null, null);
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
+
+        public int getHeight() {
+            int leftHeight = left != null ? left.getHeight() : -1;
+            int rightHeight = right != null ? right.getHeight() : -1;
+            return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+        }
+
+        public int getSize() {
+            return ((this.left != null) ? left.getSize() : 0) + 1 + ((this.right != null) ? right.getSize() : 0);
+        }
+    }
+
+    protected TreeNode root;
+    protected Comparator<? super T> comparator;
+
+    protected TreeNode addNode(TreeNode root, T data) {
+        if (root != null) {
+            if (comparator.compare(data, root.getData()) < 0)
+                root.setLeft(addNode(root.getLeft(), data));
+            else
+                root.setRight(addNode(root.getRight(), data));
+        } else
+            root = new TreeNode(data);
+        return root;
+    }
+
+    public void add(T data) {
+        this.root = addNode(this.root, data);
+    }
+
+    public BinarySearchTree(Comparator<? super T> comparator) {
+        this.root = null;
+        this.comparator = comparator;
+    }
+
+    public boolean contain(T data) {
+        TreeNode temp = this.root;
+        while (temp != null) {
+            if (comparator.compare(data, temp.getData()) < 0)
+                temp = temp.getLeft();
+            else if (comparator.compare(data, temp.getData()) > 0)
+                temp = temp.getRight();
+            else
+                return true;
+        }
+        return false;
+    }
+
+    protected TreeNode deleteNode(TreeNode root, T data) {
+        if (root != null) {
+            if (comparator.compare(data, root.getData()) < 0)
+                root.setLeft(deleteNode(root.getLeft(), data));
+            else if (comparator.compare(data, root.getData()) > 0)
+                root.setRight(deleteNode(root.getRight(), data));
+            else {
+                if (root.getLeft() != null && root.getRight() != null) {
+                    if (root.getLeft().getHeight() > root.getRight().getHeight()) {
+                        TreeNode temp = root.getLeft();
+                        while (temp.getRight() != null)
+                            temp = temp.getRight();
+                        root.setData(temp.getData());
+                        root.setLeft(deleteNode(root.getLeft(), root.getData()));
+                    } else {
+                        TreeNode temp = root.getRight();
+                        while (temp.getLeft() != null)
+                            temp = temp.getLeft();
+                        root.setData(temp.getData());
+                        root.setRight(deleteNode(root.getRight(), root.getData()));
+                    }
+                } else if (root.getLeft() != null)
+                    root = root.getLeft();
+                else
+                    root = root.getRight();
+            }
+        }
+        return root;
+    }
+
+    public boolean delete(T data) {
+        if (contain(data)) {
+            this.root = deleteNode(this.root, data);
+            return true;
+        } else
+            return false;
+    }
+
+    public int getSize() {
+        return this.root.getSize();
+    }
+
+    public T[] inOrder() {
+        Object[] array = new Object[getSize()];
+        OrderNode(this.root, array, 0, 0);
+        return (T[]) array;
+    }
+
+    public T[] preOrder() {
+        Object[] array = new Object[getSize()];
+        OrderNode(this.root, array, 0, -1);
+        return (T[]) array;
+    }
+
+    public T[] postOrder() {
+        Object[] array = new Object[getSize()];
+        OrderNode(this.root, array, 0, 1);
+        return (T[]) array;
+    }
+
+    public T[] levelOrder() {
+        Object[] array = new Object[getSize()];
+        int maxSize = 1;
+        int pos = 0;
+        Object[] queue = new Object[maxSize];
+        queue[0] = root;
+        while (queue[0] != null) {
+            Object[] temp = new Object[maxSize <<= 1];
+            int index = 0;
+            for (Object o : queue) {
+                TreeNode n = (TreeNode) o;
+                if (n == null)
+                    break;
+                else {
+                    array[pos++] = n;
+                    if (n.getLeft() != null)
+                        temp[index++] = n.getLeft();
+                    if (n.getRight() != null)
+                        temp[index++] = n.getRight();
+                }
+            }
+            queue = temp;
+        }
+        return (T[]) array;
+    }
+
+    private int OrderNode(TreeNode root, Object[] array, int pos, int pattern) {
+        if (root != null) {
+
+            if (pattern == -1)
+                array[pos++] = root.getData();
+            pos = OrderNode(root.getLeft(), array, pos, pattern);
+            if (pattern == 0)
+                array[pos++] = root.getData();
+            pos = OrderNode(root.getRight(), array, pos, pattern);
+            if (pattern == 1)
+                array[pos++] = root.getData();
+        }
+        return pos;
+    }
+
+    public void rotateLeft() {
+        if (this.root.getRight() != null) {
+            TreeNode temp = this.root.getRight();
+            this.root.setRight(this.root.getRight().getLeft());
+            temp.setLeft(this.root);
+            this.root = temp;
+        }
+    }
+
+    public void rotateRight() {
+        if (this.root.getLeft() != null) {
+            TreeNode temp = this.root.getLeft();
+            this.root.setLeft(this.root.getLeft().getRight());
+            temp.setRight(this.root);
+            this.root = temp;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sBuilder = new StringBuilder();
+        int maxSize = 1;
+        Object[] queue = new Object[maxSize];
+        queue[0] = root;
+        while (queue[0] != null) {
+            Object[] temp = new Object[maxSize <<= 1];
+            int index = 0;
+            for (Object o : queue) {
+                TreeNode n = (TreeNode) o;
+                if (n == null)
+                    break;
+                else {
+                    sBuilder.append(n.getData().toString()).append(" ");
+                    if (n.getLeft() != null)
+                        temp[index++] = n.getLeft();
+                    if (n.getRight() != null)
+                        temp[index++] = n.getRight();
+                }
+            }
+            sBuilder.append("\n");
+            queue = temp;
+        }
+        return sBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> t = new BinarySearchTree<Integer>((Integer data1, Integer data2) -> {
+            return Integer.compare(data1, data2);
+        });
+        t.add(10);
+        t.add(7);
+        t.add(110);
+        t.add(32);
+        t.add(10);
+        t.add(9);
+        t.add(1);
+        t.add(45);
+        System.out.println(java.util.Arrays.toString(t.inOrder()));
+        System.out.println(java.util.Arrays.toString(t.preOrder()));
+        System.out.println(java.util.Arrays.toString(t.postOrder()));
+        System.out.println(java.util.Arrays.toString(t.levelOrder()));
+        System.out.println(t);
+        t.rotateRight();
+        System.out.println(t);
+        t.rotateRight();
+        System.out.println(t);   
+             
+    }
+}
